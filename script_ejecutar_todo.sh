3cf213187b998b66fbd4f6e4c045b6b60c675f61
esgonzalezca <36159488+esgonzalezca@users.noreply.github.com>
2021-12-13 10:08:12 -0500

    Update script_ejecutar_todo.sh

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 
-nvcc taller3.cu -o taller3 -g `pkg-config --cflags --libs opencv`
+nvcc taller3.cu -o t3 -g `pkg-config --cflags --libs opencv`
 
 ./t3 720p.jpg 720psobel.jpg 1 16
-./t3 720p.jpg 720psobel.jpg 1 64
-./t3 720p.jpg 720psobel.jpg 1 128
-./t3 720p.jpg 720psobel.jpg 12 64
-./t3 720p.jpg 720psobel.jpg 12 128
-./t3 720p.jpg 720psobel.jpg 24 128
-./t3 720p.jpg 720psobel.jpg 48 128
-./t3 720p.jpg 720psobel.jpg 24 256
-./t3 720p.jpg 720psobel.jpg 96 128
-./t3 720p.jpg 720psobel.jpg 24 512
-./t3 720p.jpg 720psobel.jpg 96 512
-./t3 720p.jpg 720psobel.jpg 240 1280
+./t3 720p.jpg 720psobel.jpg 1 17
+./t3 720p.jpg 720psobel.jpg 1 18
+./t3 720p.jpg 720psobel.jpg 1 19
+./t3 720p.jpg 720psobel.jpg 1 20
+./t3 720p.jpg 720psobel.jpg 2 32
+./t3 720p.jpg 720psobel.jpg 2 64
+./t3 720p.jpg 720psobel.jpg 2 128
 
-./t3 1080p.jpg 1080psobel.jpg 1 16
+
+./t3 1080p.jpg 1080psobel.jpg 1 32
 ./t3 1080p.jpg 1080psobel.jpg 1 64
 ./t3 1080p.jpg 1080psobel.jpg 1 128
-./t3 1080p.jpg 1080psobel.jpg 12 64
-./t3 1080p.jpg 1080psobel.jpg 12 128
-./t3 1080p.jpg 1080psobel.jpg 24 128
-./t3 1080p.jpg 1080psobel.jpg 48 128
-./t3 1080p.jpg 1080psobel.jpg 24 256
-./t3 1080p.jpg 1080psobel.jpg 96 128
-./t3 1080p.jpg 1080psobel.jpg 24 512
-./t3 1080p.jpg 1080psobel.jpg 96 512
-./t3 1080p.jpg 1080psobel.jpg 240 1280
+./t3 1080p.jpg 1080psobel.jpg 2 16
+./t3 1080p.jpg 1080psobel.jpg 2 32
+./t3 1080p.jpg 1080psobel.jpg 2 64
+./t3 1080p.jpg 1080psobel.jpg 2 128
+./t3 1080p.jpg 1080psobel.jpg 3 64
+./t3 1080p.jpg 1080psobel.jpg 3 128
+./t3 1080p.jpg 1080psobel.jpg 4 64
+./t3 1080p.jpg 1080psobel.jpg 4 128
 
-./t3 4k.jpg 4ksobel.jpg 1 16
-./t3 4k.jpg 4ksobel.jpg 1 64
+
 ./t3 4k.jpg 4ksobel.jpg 1 128
-./t3 4k.jpg 4ksobel.jpg 12 64
+./t3 4k.jpg 4ksobel.jpg 2 128
+./t3 4k.jpg 4ksobel.jpg 3 128
+./t3 4k.jpg 4ksobel.jpg 4 128
+./t3 4k.jpg 4ksobel.jpg 5 128
+./t3 4k.jpg 4ksobel.jpg 6 128
+./t3 4k.jpg 4ksobel.jpg 7 128
+./t3 4k.jpg 4ksobel.jpg 8 128
+./t3 4k.jpg 4ksobel.jpg 9 128
+./t3 4k.jpg 4ksobel.jpg 10 128
 ./t3 4k.jpg 4ksobel.jpg 12 128
-./t3 4k.jpg 4ksobel.jpg 24 128
-./t3 4k.jpg 4ksobel.jpg 48 128
-./t3 4k.jpg 4ksobel.jpg 24 256
-./t3 4k.jpg 4ksobel.jpg 96 128
-./t3 4k.jpg 4ksobel.jpg 24 512
-./t3 4k.jpg 4ksobel.jpg 96 512
-./t3 4k.jpg 4ksobel.jpg 240 1280
+./t3 4k.jpg 4ksobel.jpg 256 512
