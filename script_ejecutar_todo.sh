2019c251ca5ed14183eb41abbf3475abbea459cb
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2021-12-12 20:45:34 -0500

    Update script_ejecutar_todo.sh

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -2,17 +2,14 @@
 
 nvcc taller3.cu -o t3 -fopenmp `pkg-config --cflags --libs opencv` 
 
-./t3 720p.jpg 720psobel.jpg 1 1
-./t3 720p.jpg 720psobel.jpg 1 1
-./t3 720p.jpg 720psobel.jpg 1 1
-./t3 720p.jpg 720psobel.jpg 1 1
+./t3 720p.jpg 1 1
+./t3 720p.jpg 1 1
+./t3 720p.jpg 1 1
 
-./t3 1080p.jpg 1080psobel.jpg 1 1
-./t3 1080p.jpg 1080psobel.jpg 1 1
-./t3 1080p.jpg 1080psobel.jpg 1 1
-./t3 1080p.jpg 1080psobel.jpg 1 1
+./t3 1080p.jpg 1 1
+./t3 1080p.jpg 1 1
+./t3 1080p.jpg 1 1
 
-./t3 4k.jpg 4ksobel.jpg 1 1
-./t3 4k.jpg 4ksobel.jpg 1 1
-./t3 4k.jpg 4ksobel.jpg 1 1
-./t3 4k.jpg 4ksobel.jpg 1 1
+./t3 4k.jpg 1 1
+./t3 4k.jpg 1 1
+./t3 4k.jpg 1 1
