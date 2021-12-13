9d59c7ba52f23c127c3b15d44c6ddc325068cb69
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2021-12-12 20:43:33 -0500

    Update script_ejecutar_todo.sh

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-g++ taller2.cpp -o t2 -fopenmp `pkg-config --cflags --libs opencv` 
+nvcc taller3.cu -o t3 -fopenmp `pkg-config --cflags --libs opencv` 
 
-./t2 720p.jpg 720psobel.jpg 2
-./t2 720p.jpg 720psobel.jpg 4
-./t2 720p.jpg 720psobel.jpg 8
-./t2 720p.jpg 720psobel.jpg 16
+./t3 720p.jpg 720psobel.jpg 1 1
+./t3 720p.jpg 720psobel.jpg 1 1
+./t3 720p.jpg 720psobel.jpg 1 1
+./t3 720p.jpg 720psobel.jpg 1 1
 
-./t2 1080p.jpg 1080psobel.jpg 2
-./t2 1080p.jpg 1080psobel.jpg 4
-./t2 1080p.jpg 1080psobel.jpg 8
-./t2 1080p.jpg 1080psobel.jpg 16
+./t3 1080p.jpg 1080psobel.jpg 1 1
+./t3 1080p.jpg 1080psobel.jpg 1 1
+./t3 1080p.jpg 1080psobel.jpg 1 1
+./t3 1080p.jpg 1080psobel.jpg 1 1
 
-./t2 4k.jpg 4ksobel.jpg 2
-./t2 4k.jpg 4ksobel.jpg 4
-./t2 4k.jpg 4ksobel.jpg 8
-./t2 4k.jpg 4ksobel.jpg 16
+./t3 4k.jpg 4ksobel.jpg 1 1
+./t3 4k.jpg 4ksobel.jpg 1 1
+./t3 4k.jpg 4ksobel.jpg 1 1
+./t3 4k.jpg 4ksobel.jpg 1 1
