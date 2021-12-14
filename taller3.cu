2046685556e686ca9f0f906a1180341da1c3658a
htovars <56688304+htovars@users.noreply.github.com>
2021-12-14 09:54:30 -0500

    Update taller3.cu

--- taller3.cu
+++ taller3.cu
@@ -27,12 +27,12 @@ __global__ void sobel(unsigned char *d_imgGray, unsigned char *d_imgSobel, int c
 	
 if (index==0){
 	
-__shared__ Kernel[3][3] = {
+__shared__ float Kernel[3][3] = {
                  {-1, 0, 1},
                  {-2, 0, 2},
                  {-1, 0, 1}
     };
-__shared__ Kernel2[3][3] = {
+__shared__ float Kernel2[3][3] = {
                     {-1, -2, -1},
                     {0, 0, 0},
                     {1, 2, 1}
